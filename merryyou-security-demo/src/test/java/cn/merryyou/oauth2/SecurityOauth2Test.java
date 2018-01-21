61302b748854564bce22f9117cd69a44f9b9a6a3
longfeizheng <longfei_zheng@163.com>
2018-01-21 16:49:16 +0800

    添加测试类返回用户信息

---
+++ merryyou-security-demo/src/test/java/cn/merryyou/oauth2/SecurityOauth2Test.java
@@ -0,0 +1,84 @@
+package cn.merryyou.oauth2;
+
+import cn.merryyou.utils.JsonUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.codec.binary.Base64;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.*;
+import org.springframework.security.oauth2.common.OAuth2AccessToken;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * Created on 2018/1/17.
+ *
+ * @author zlf
+ * @since 1.0
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Slf4j
+public class SecurityOauth2Test {
+    //端口
+    final static long PORT = 8060;
+    //clientId
+    final static String CLIENT_ID = "merryyou";
+    //clientSecret
+    final static String CLIENT_SECRET = "merryyousecret";
+    //用户名
+    final static String USERNAME = "admin";
+    //密码
+    final static String PASSWORD = "123456";
+    //获取accessToken得URI
+    final static String TOKEN_REQUEST_URI = "http://127.0.0.1:" + PORT + "/oauth/token?grant_type=password&username=" + USERNAME + "&password=" + PASSWORD + "&scope=all";
+    //获取用户信息得URL
+    final static String USER_INFO_URI = "http://127.0.0.1:" + PORT + "/user/me";
+
+    @Test
+    public void getUserInfo() throws Exception {
+        RestTemplate rest = new RestTemplate();
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("authorization", "Bearer " + getAccessToken());
+        HttpEntity<String> entity = new HttpEntity<String>(null, headers);
+        // pay attention, if using get with headers, should use exchange instead of getForEntity / getForObject
+        ResponseEntity<String> result = rest.exchange(USER_INFO_URI, HttpMethod.GET, entity, String.class, new Object[]{null});
+        log.info("用户信息返回的结果={}", result);
+    }
+
+    /**
+     * 获取accessToken
+     *
+     * @return
+     */
+    private String getAccessToken() {
+        RestTemplate rest = new RestTemplate();
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.TEXT_PLAIN);
+        headers.add("authorization", getBasicAuthHeader());
+        HttpEntity<String> entity = new HttpEntity<String>(null, headers);
+        ResponseEntity<OAuth2AccessToken> resp = rest.postForEntity(TOKEN_REQUEST_URI, entity, OAuth2AccessToken.class);
+        if (!resp.getStatusCode().equals(HttpStatus.OK)) {
+            throw new RuntimeException(resp.toString());
+        }
+        OAuth2AccessToken t = resp.getBody();
+        log.info("accessToken={}", JsonUtil.toJson(t));
+        log.info("the response, access_token: " + t.getValue() + "; token_type: " + t.getTokenType() + "; "
+                + "refresh_token: " + t.getRefreshToken() + "; expiration: " + t.getExpiresIn() + ", expired when:" + t.getExpiration());
+        return t.getValue();
+
+    }
+
+    /**
+     * 构建header
+     *
+     * @return
+     */
+    private String getBasicAuthHeader() {
+        String auth = CLIENT_ID + ":" + CLIENT_SECRET;
+        byte[] encodedAuth = Base64.encodeBase64(auth.getBytes());
+        String authHeader = "Basic " + new String(encodedAuth);
+        return authHeader;
+    }
+}
